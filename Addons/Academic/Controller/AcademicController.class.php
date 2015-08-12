ed545cd9cefe80a408ffb21cb65c688bc2a30086
skyekeven <skyekeven@gmail.com>
2015-08-12 20:20:57 +0800

    save

--- Addons/Academic/Controller/AcademicController.class.php
+++ Addons/Academic/Controller/AcademicController.class.php
@@ -4,5 +4,7 @@ namespace Addons\Academic\Controller;
 use Home\Controller\AddonsController;
 
 class AcademicController extends AddonsController{
-
+	public function  instruction(){
+		$this->error("失败了哦");
+	}
 }
