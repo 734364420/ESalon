c7eab309e7332ecddba21d2237cb2bbdcaad7f40
skyekeven <skyekeven@gmail.com>
2015-08-12 20:34:41 +0800

    save

--- Addons/Academic/Controller/AcademicController.class.php
+++ Addons/Academic/Controller/AcademicController.class.php
@@ -4,6 +4,10 @@ namespace Addons\Academic\Controller;
 use Home\Controller\AddonsController;
 
 class AcademicController extends AddonsController{
+	public function __construct() {
+		parent::__construct();
+		$openid = $_GET['openid'];
+	}
 	function  instruction() {
 		$this->success("成功了哦");
 	}
