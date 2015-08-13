305db7a8ab16968c7de62dbbfa2f8ae9083a2561
llluo <1979707287@qq.com>
2015-08-13 16:15:06 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -4,7 +4,7 @@ use Home\Controller\AddonsController;
 class SalonController extends AddonsController{
 	public function __construct() {
 		parent::__construct();
-		$openid = $_GET['openid'];
+		$openid = I('openid');
 		//e_auth($openid);
 	}
 
@@ -15,9 +15,23 @@ class SalonController extends AddonsController{
 	function MySalon() {
 		$this->display('Salon/instruction');
 	}
-
+	//发起E沙龙模块
 	function CreateSalon() {
-		$this->display();
+		if(IS_POST) {
+			$data['title']='好好';
+			$data['date']='haohao';
+			$data['space']='haohao';
+			$data['type']='haohao';
+			$data['brief']='haohao';
+			$result=$db=M('e_salon')->add($data);
+			if($result){
+				$this->success('添加成功',addons_url('Salon://Salon/instrucion'),3);
+			}else{
+				$this->error('添加失败，请检查原因');
+			}
+		} else {
+			$this->display();
+		}
 	}
 
 	function SalonSquare() {
