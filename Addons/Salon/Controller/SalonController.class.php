5708789b3c4d731710796d5f3b5c0c1b5e336a8c
llluo <1979707287@qq.com>
2015-08-13 12:47:18 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -8,7 +8,16 @@ class SalonController extends AddonsController{
 		parent::__construct();
 		$openid = $_GET['openid'];
 	}
+
 	function  instruction() {
 		$this->display();
+	}
+
+	function mysalon() {
+		if(!$openid){
+			$this->display('Salon/instruction');
+		}else{
+			$this->display('Salon/addinformation');
+		}
 	}
 }
