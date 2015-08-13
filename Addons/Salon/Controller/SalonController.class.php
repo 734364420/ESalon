454152b787e945c2b63962ee4830a6bd0d580a54
llluo <1979707287@qq.com>
2015-08-13 13:04:44 +0800

    delete iade

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -7,17 +7,14 @@ class SalonController extends AddonsController{
 	public function __construct() {
 		parent::__construct();
 		$openid = $_GET['openid'];
+		e_auth($openid);
 	}
 
 	function  instruction() {
 		$this->display();
 	}
 
-	function mysalon() {
-		if(!$openid){
-			$this->display('Salon/instruction');
-		}else{
-			$this->display('Salon/addinformation');
-		}
+	function MySalon() {
+		$this->display('Salon/instruction');
 	}
 }
