568781b96f8f8b1569c593c252e78e9548134ab2
HivenKay <HivenKay@yunchuangdeiMac.local>
2015-08-13 10:35:14 +0800

    修改学术推送的图文消息

--- Addons/Academic/Model/WeixinAddonModel.class.php
+++ Addons/Academic/Model/WeixinAddonModel.class.php
@@ -11,10 +11,8 @@ class WeixinAddonModel extends WeixinModel{
 		$config = getAddonConfig ( 'Academic' ); // 获取后台插件的配置参数	
         $param ['token'] = get_token ();
         $param ['openid'] = get_openid ();
-		$news = M('');
-        $url = addons_url ( 'Suggestions://Suggestions/suggest', $param );
-		//初始化查找条件，51，52，。。。55分别为E学术几个固定的图文项
-		$map_news['id'] = array('in',array(51,52,53,54,55));
+		//初始化查找条件，56,57，。。。60分别为E学术几个固定的图文项
+		$map_news['id'] = array('in',array(56,57,58,59,60));
 		$list = M ( 'custom_reply_news' )->where ( $map_news )->select ();
 		foreach ( $list as $k => $info ) {
 			if ($k > 8)
@@ -24,7 +22,7 @@ class WeixinAddonModel extends WeixinModel{
 				'Title' => $info ['title'],
 				'Description' => $info ['intro'],
 				'PicUrl' => get_cover_url ( $info ['cover'] ),
-				'Url' => addons_url ( 'Academic://Academic/instruction', $param )
+				'Url' => addons_url ($info['jump_url'], $param )
 			);
 		}
         $res = $this->replyNews ( $articles );
