c6fde7e3009e4ca34b11becf26098c3a393ff1f6
llluo <1979707287@qq.com>
2015-08-14 21:36:38 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -175,7 +175,7 @@ class SalonController extends AddonsController{
 		$param ['token'] = get_token ();
 		$param ['openid'] = get_openid ();
 		if($result){
-			$this->success('参加成功',addons_url('Salon://Salon/ParticipateSalon',$param),3);
+			$this->success('参加成功',addons_url('Salon://Salon/SalonSquare',$param),3);
 		}else{
 			$this->error('参加失败');
 		}
