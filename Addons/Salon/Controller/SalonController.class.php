7fdb532dd109b51c562c3428f6d5216f9a2911d6
llluo <1979707287@qq.com>
2015-08-14 15:42:52 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -70,8 +70,6 @@ class SalonController extends AddonsController{
 			$data['brief']=\LfRequest::inStr('brief');
 			$data['publish_userid']=session('user_id');
 			$data['hits']=0;
-			var_dump($data);
-			die();
 			$user = M('e_salon');
 			$result=$user->add($data);
 			if($result){
