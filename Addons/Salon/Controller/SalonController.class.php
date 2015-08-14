cae4fde826e379a00abbf34afc2e674321ca65c2
llluo <1979707287@qq.com>
2015-08-14 21:50:01 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -63,12 +63,14 @@ class SalonController extends AddonsController{
 			$this->display();
 
 		}else {
+			$db=M('e_salon');
 			$user = M('e_user')->where('id=' . session('user_id'))->getField('student_name');
 			$salons_publish = M('e_salon')->where('publish_userid=' . session('user_id'))->select();
 			$participattions = M('e_participate')->where('user_id=' . session('user_id'))->select();
 			var_dump($participattions);
 			for ($i = 0,$j = 0; $i < count($participattions); $i++) {
-				$result= M('e_salon')->where("'id='.$participattions[$i]['e_id'].' AND publish_userid!='.session('user_id')")->find();
+				$result= M('e_salon')->where('id='.$participattions[$i]['e_id'].' AND publish_userid!='.session('user_id'))->find();
+				echo $db->getLastSql();
 				var_dump($result);
 				if($result){
 					$salons_participate[$j]=$result;
