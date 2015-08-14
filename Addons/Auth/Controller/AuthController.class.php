95fe0613240d6eb9431faffefa4dedaff4935507
HivenKay <HivenKay@yunchuangdeiMac.local>
2015-08-14 09:55:37 +0800

    添加后台学生认证信息列表查看

--- Addons/Auth/Controller/AuthController.class.php
+++ Addons/Auth/Controller/AuthController.class.php
@@ -4,6 +4,12 @@ namespace Addons\Auth\Controller;
 use Home\Controller\AddonsController;
 
 class AuthController extends AddonsController{
+    function __construct() {
+        parent::__construct();
+        $this->model = M ( 'Model' )->getByName ( $_REQUEST ['_controller'] );
+        $this->model || $this->error ( '模型不存在！' );
+        $this->assign ( 'model', $this->model );
+    }
     function Auth() {
         if(IS_POST) {
             $user = M('e_user');
@@ -71,5 +77,53 @@ class AuthController extends AddonsController{
             $this->display('Auth/Auth');
         }
 
+    }
+    //list列表，已认证学生列表
+    function lists() {
+        $users = M('e_user');
+        $page = I ( 'p', 1, 'intval' ); // 默认显示第一页数据
+
+        // 解析列表规则
+        $list_data = $this->_list_grid ( $this->model );
+        $grids = $list_data ['list_grids'];
+        $fields = $list_data ['fields'];
+
+        // 关键字搜索
+        $map ['token'] = get_token ();
+        $key = $this->model ['search_key'] ? $this->model ['search_key'] : 'title';
+        if (isset ( $_REQUEST [$key] )) {
+            $map [$key] = array (
+                'like',
+                '%' . htmlspecialchars ( $_REQUEST [$key] ) . '%'
+            );
+            unset ( $_REQUEST [$key] );
+        }
+        // 条件搜索
+        foreach ( $_REQUEST as $name => $val ) {
+            if (in_array ( $name, $fields )) {
+                $map [$name] = $val;
+            }
+        }
+        $row = empty ( $this->model ['list_row'] ) ? 20 : $this->model ['list_row'];
+
+        // 读取模型数据列表
+
+        empty ( $fields ) || in_array ( 'id', $fields ) || array_push ( $fields, 'id' );
+        $name = parse_name ( get_table_name ( $this->model ['id'] ), true );
+        $data = M ( $name )->field ( empty ( $fields ) ? true : $fields )->where ( $map )->order ( 'id DESC' )->page ( $page, $row )->select ();
+
+        /* 查询记录总数 */
+        $count = M ( $name )->where ( $map )->count ();
+
+        // 分页
+        if ($count > $row) {
+            $page = new \Think\Page ( $count, $row );
+            $page->setConfig ( 'theme', '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%' );
+            $this->assign ( '_page', $page->show () );
+        }
+
+        $this->assign ( 'list_grids', $grids );
+        $this->assign ( 'list_data', $data );
+        $this->display();
     }
 }
