aad1963d4236c4550c77bde50b96d5c72331d4fd
llluo <1979707287@qq.com>
2015-08-14 20:19:49 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -36,10 +36,7 @@ class SalonController extends AddonsController{
 				}
 			}
 			$data['publish_userid']=session('user_id');
-			var_dump($data);
-			echo '===========';
 			$salons_publish = M('e_salon')->where($data)->select();
-			var_dump($salons_publish);
 			for ($i = 0,$j = 0; $i < count($participattions); $i++) {
 				$result= M('e_salon')->where('id='.$participattions[$i]['e_id'].'AND publish_userid!='.session('user_id'))->find();
 				if($result){
