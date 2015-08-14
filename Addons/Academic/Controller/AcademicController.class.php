e7ce98271bc12336d50697bfaf58e8c046ea45e7
skyekeven <skyekeven@gmail.com>
2015-08-14 19:01:25 +0800

    save

--- Addons/Academic/Controller/AcademicController.class.php
+++ Addons/Academic/Controller/AcademicController.class.php
@@ -38,11 +38,11 @@ class AcademicController extends AddonsController{
 		    if(!empty($data['type'])) {
 			    $maps .= ' type = '.$data['type'];
 		    }
-		    if($data['iteam_status'] == 0) {
-			    $maps .= ' end_date > '.date("Y-m-d");
+		    if($data['iteam_status'] == 0 && $data['iteam_status'] != '' ) {
+			    $maps .= ' end_date > '.time();
 		    }
 		    if($data['iteam_status'] == 1) {
-			    $maps .= ' end_date < '.date("Y-m-d");
+			    $maps .= ' end_date < '.time();
 		    }
 		    if($data['summary_status'] != '') {
 			    $maps .= ' summary_status = '.$data['summary_status'];
@@ -74,8 +74,8 @@ class AcademicController extends AddonsController{
             }
             $iteam = M('e_iteam');
             $iteam->title = $title;
-            $iteam->start_date = I('start_date');
-            $iteam->end_date = I('end_date');
+            $iteam->start_date = strtotime(I('start_date'));
+            $iteam->end_date = strtotime(I('end_date'));
             $iteam->participate_number = I('participate_number');
             $iteam->type = I('type');
             $iteam->brief = I('brief');
