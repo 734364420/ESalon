15733b380fc228fd59be2a356a7515f21080293b
llluo <1979707287@qq.com>
2015-08-14 14:38:04 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -63,7 +63,7 @@ class SalonController extends AddonsController{
 			$time=\LfRequest::inStr('time');
 			$hour=\LfRequest::inStr('hour');
 			$hour=str_replace("h","",$hour);
-			$data['date']=$date.' '.$time.'~'.date('H:i',strtotime($time,'+'.$hour.' hour'));
+			$data['date']=$date.' '.$time.'~'.date('H:i',strtotime($time,'-'.$hour.' hour'));
 			$data['space']=\LfRequest::inStr('space');
 			$data['participate_number']=\LfRequest::inNum('participate_number');
 			$data['type']=\LfRequest::inStr('type');
