e6f4e5c8bd2065978fce7c53bcf5a1d09883907f
llluo <1979707287@qq.com>
2015-08-15 12:17:39 +0800

    repair the CreateSalon

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -97,7 +97,6 @@ class SalonController extends AddonsController{
 	//查看发布沙龙详细信息
 	function CheckSalon() {
 		$id=\LfRequest::inNum('id');
-		var_dump($id);
 		$salon=M('e_salon')->where('id='.$id)->find();
 		$data['hits']=$salon['hits']+1;
 		M('e_salon')->where('id='.$id)->save($data);
