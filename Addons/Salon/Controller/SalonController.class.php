809ae3efc1697c042067d7842419c98e90dd4a00
llluo <1979707287@qq.com>
2015-08-15 12:18:43 +0800

    repair the CreateSalon

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -103,6 +103,7 @@ class SalonController extends AddonsController{
 		$this->salon=$salon;
 		$this->publish_user=M('e_user')->where('id='.$salon['publish_userid'])->find();
 		$participate_users=M('e_participate')->where('e_id='.$id)->select();
+		var_dump($participate_users);
 		for($i=0;$i<count($participate_users);$i++){
 			var_dump($participate_users[$i]['user_id']);
 			$participate_users[$i]=M('e_user')->where('id='.$participate_users[$i]['user_id'])->find();
