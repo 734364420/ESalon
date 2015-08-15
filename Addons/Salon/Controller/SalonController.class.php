560ad195f42a14e44ea40af6842dc8ac2d0187f1
llluo <1979707287@qq.com>
2015-08-15 13:05:45 +0800

    repair the CreateSalon

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -17,8 +17,6 @@ class SalonController extends AddonsController{
 			$type=\LfRequest::inStr('type');
 			$salon_status=\LfRequest::inNum('salon_status');
 			$salon_summary_status=\LfRequest::inNum('salon_summary_status');
-			var_dump($salon_status);
-			var_dump($salon_summary_status);
 			$today = date('Y-m-d',time());
 			if($type != null) {
 				$data['type'] = $type;
@@ -40,7 +38,6 @@ class SalonController extends AddonsController{
 			$data['publish_userid']=session('user_id');
 			$user=M('e_salon');
 			$salons_publish = M('e_salon')->where($data)->select();
-			echo $user->getLastSql();
 			for ($i = 0,$j = 0; $i < count($participattions); $i++) {
 				$result= M('e_salon')->where('id='.$participattions[$i]['e_id'].' AND publish_userid!='.session('user_id'))->find();
 				if($result){
