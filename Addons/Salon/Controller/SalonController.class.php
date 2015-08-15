de6d4e886d04aecc8215ccaaa14fc163a6168c00
llluo <1979707287@qq.com>
2015-08-15 21:02:21 +0800

    整合模板完成

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -4,6 +4,8 @@ use Home\Controller\AddonsController;
 class SalonController extends AddonsController{
 	public function __construct() {
 		parent::__construct();
+		$this->model = M('Model')->getByName('e_salon');
+		$this->assign ( 'model', $this->model );
 	}
 
 	function  Instruction() {
@@ -233,5 +235,77 @@ class SalonController extends AddonsController{
 		}
 		$this->assign('title','查询结果');
 		$this->display('Salon/SalonSquare');
+	}
+
+	function lists() {
+		$users = M('e_user');
+		$page = I ( 'p', 1, 'intval' ); // 默认显示第一页数据
+
+		// 解析列表规则
+		$list_data = $this->_list_grid ( $this->model );
+		$grids = $list_data ['list_grids'];
+		$fields = $list_data ['fields'];
+
+		// 关键字搜索
+		$map ['token'] = get_token ();
+		$key = $this->model ['search_key'] ? $this->model ['search_key'] : 'title';
+		if (isset ( $_REQUEST [$key] )) {
+			$map [$key] = array (
+				'like',
+				'%' . htmlspecialchars ( $_REQUEST [$key] ) . '%'
+			);
+			unset ( $_REQUEST [$key] );
+		}
+		// 条件搜索
+		foreach ( $_REQUEST as $name => $val ) {
+			if (in_array ( $name, $fields )) {
+				$map [$name] = $val;
+			}
+		}
+		$row = empty ( $this->model ['list_row'] ) ? 20 : $this->model ['list_row'];
+
+		// 读取模型数据列表
+
+		empty ( $fields ) || in_array ( 'id', $fields ) || array_push ( $fields, 'id' );
+		$name = parse_name ( get_table_name ( $this->model ['id'] ), true );
+		$data = M ( $name )->field ( empty ( $fields ) ? true : $fields )->where ( $map )->order ( 'id DESC' )->page ( $page, $row )->select ();
+
+		/* 查询记录总数 */
+		$count = M ( $name )->where ( $map )->count ();
+
+		// 分页
+		if ($count > $row) {
+			$page = new \Think\Page ( $count, $row );
+			$page->setConfig ( 'theme', '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%' );
+			$this->assign ( '_page', $page->show () );
+		}
+
+		$this->assign ( 'list_grids', $grids );
+		$this->assign ( 'list_data', $data );
+		$this->assign ( 'publish', '1' );
+		$this->display();
+	}
+	public function del() {
+		$ids = I ( 'id', 0 );
+		if (empty ( $ids )) {
+			$ids = array_unique ( ( array ) I ( 'ids', 0 ) );
+		}
+		if (empty ( $ids )) {
+			$this->error ( '请选择要操作的数据!' );
+		}
+
+		$Model = M ( get_table_name ( $this->model ['id'] ) );
+		$map = array (
+			'id' => array (
+				'in',
+				$ids
+			)
+		);
+		$map ['token'] = get_token ();
+		if ($Model->where ( $map )->delete ()) {
+			$this->success ( '删除成功' );
+		} else {
+			$this->error ( '删除失败！' );
+		}
 	}
 }
