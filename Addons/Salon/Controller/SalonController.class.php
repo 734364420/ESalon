c37c4c2fd7db83d878f5890524ebf34cdf834290
llluo <1979707287@qq.com>
2015-08-16 15:05:13 +0800

    save

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -18,28 +18,29 @@ class SalonController extends AddonsController{
 //		$run->star();
 		$this->assign('url','Salon://Salon/CheckSalon');
 		$participattions=M('e_participate')->where('user_id='.session('user_id'))->select();
-	if(IS_POST){
-		$type=\LfRequest::inStr('type');
-		$salon_status=\LfRequest::inNum('salon_status');
-		$salon_summary_status=\LfRequest::inNum('salon_summary_status');
-		$today = date('Y-m-d',time());
-		if($type != null) {
+	if(IS_POST) {
+		$type = \LfRequest::inStr('type');
+		$salon_status = \LfRequest::inNum('salon_status');
+		$salon_summary_status = \LfRequest::inNum('salon_summary_status');
+		$today = date('Y-m-d', time());
+		if ($type != null) {
 			$data['type'] = $type;
 		}
-		if(!empty($salon_status)){
-			if($salon_status == 1){
-				$data['end_date']=array('lt',strtotime($today));
-			}elseif($salon_status == 2){
-				$data['end_date']=array('egt',strtotime($today));
+		if (!empty($salon_status)) {
+			if ($salon_status == 1) {
+				$data['end_date'] = array('lt', strtotime($today));
+			} elseif ($salon_status == 2) {
+				$data['end_date'] = array('egt', strtotime($today));
 			}
 		}
-		if(!empty($salon_summary_status)) {
+		if (!empty($salon_summary_status)) {
 			if ($salon_summary_status == 1) {
 				$data['summary'] = 1;
 			} elseif ($salon_summary_status == 2) {
 				$data['summary'] = 0;
 			}
 		}
+	}
 		$data['publish_userid']=session('user_id');
 		$in='(0';
 		foreach($participattions as $v) {
@@ -231,52 +232,53 @@ class SalonController extends AddonsController{
 		$this->display('Salon/SalonSquare');
 	}
 
-	function lists() {
+	function lists()
+	{
 		$users = M('e_user');
-		$page = I ( 'p', 1, 'intval' ); // 默认显示第一页数据
+		$page = I('p', 1, 'intval'); // 默认显示第一页数据
 
 		// 解析列表规则
-		$list_data = $this->_list_grid ( $this->model );
+		$list_data = $this->_list_grid($this->model);
 		$grids = $list_data ['list_grids'];
 		$fields = $list_data ['fields'];
 
 		// 关键字搜索
-		$map ['token'] = get_token ();
+		$map ['token'] = get_token();
 		$key = $this->model ['search_key'] ? $this->model ['search_key'] : 'title';
-		if (isset ( $_REQUEST [$key] )) {
-			$map [$key] = array (
+		if (isset ($_REQUEST [$key])) {
+			$map [$key] = array(
 				'like',
-				'%' . htmlspecialchars ( $_REQUEST [$key] ) . '%'
+				'%' . htmlspecialchars($_REQUEST [$key]) . '%'
 			);
-			unset ( $_REQUEST [$key] );
+			unset ($_REQUEST [$key]);
 		}
 		// 条件搜索
-		foreach ( $_REQUEST as $name => $val ) {
-			if (in_array ( $name, $fields )) {
+		foreach ($_REQUEST as $name => $val) {
+			if (in_array($name, $fields)) {
 				$map [$name] = $val;
 			}
 		}
-		$row = empty ( $this->model ['list_row'] ) ? 20 : $this->model ['list_row'];
+		$row = empty ($this->model ['list_row']) ? 20 : $this->model ['list_row'];
 
 		// 读取模型数据列表
 
-		empty ( $fields ) || in_array ( 'id', $fields ) || array_push ( $fields, 'id' );
-		$name = parse_name ( get_table_name ( $this->model ['id'] ), true );
-		$data = M ( $name )->field ( empty ( $fields ) ? true : $fields )->where ( $map )->order ( 'id DESC' )->page ( $page, $row )->select ();
+		empty ($fields) || in_array('id', $fields) || array_push($fields, 'id');
+		$name = parse_name(get_table_name($this->model ['id']), true);
+		$data = M($name)->field(empty ($fields) ? true : $fields)->where($map)->order('id DESC')->page($page, $row)->select();
 
 		/* 查询记录总数 */
-		$count = M ( $name )->where ( $map )->count ();
+		$count = M($name)->where($map)->count();
 
 		// 分页
 		if ($count > $row) {
-			$page = new \Think\Page ( $count, $row );
-			$page->setConfig ( 'theme', '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%' );
-			$this->assign ( '_page', $page->show () );
+			$page = new \Think\Page ($count, $row);
+			$page->setConfig('theme', '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
+			$this->assign('_page', $page->show());
 		}
 
-		$this->assign ( 'list_grids', $grids );
-		$this->assign ( 'list_data', $data );
-		$this->assign ( 'publish', '1' );
+		$this->assign('list_grids', $grids);
+		$this->assign('list_data', $data);
+		$this->assign('publish', '1');
 		$this->display();
 	}
 	public function del() {
