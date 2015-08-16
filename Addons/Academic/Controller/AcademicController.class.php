7df595cb3ac597d6134645e4ebf4964ff7fddb5e
llluo <1979707287@qq.com>
2015-08-16 21:38:11 +0800

    调试沙龙广场

--- Addons/Academic/Controller/AcademicController.class.php
+++ Addons/Academic/Controller/AcademicController.class.php
@@ -313,7 +313,9 @@ class AcademicController extends AddonsController
 			}
 			$summary->stars = \LfRequest::inNum('stars');
 			$summary->comment = \LfRequest::inStr('comment');
-			$summary->picture = \LfRequest::inStr('picture');
+			$upload =new \LfUpload('/Picture');
+			$path = $upload->upload('file');
+			$summary->picture = $path;
 			$res = $summary->add();
 			if ($res) {
 				/*
