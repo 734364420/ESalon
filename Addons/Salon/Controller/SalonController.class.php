dfc930fdfde9c909fa0fdb2c3528b6999edcc200
llluo <1979707287@qq.com>
2015-08-16 16:56:11 +0800

    调试沙龙广场

--- Addons/Salon/Controller/SalonController.class.php
+++ Addons/Salon/Controller/SalonController.class.php
@@ -52,8 +52,8 @@ class SalonController extends AddonsController{
 		$salons_publish = M('e_salon')->where($Pdata)->count();
 		$salons_participate= M('e_salon')->where($data)->count();
 
-		$salons_publish = \LfPageData::Page($salons_publish,addons_url('Salon://Salon/MySalon/status/sign'));
-		$salons_participate = \LfPageData::Page($salons_participate,addons_url('Salon://Salon/MySalon/status/end'));
+		$this->PublishPage = \LfPageData::Page($salons_publish,addons_url('Salon://Salon/MySalon/status/sign'));
+		$this->ParticipatePage = \LfPageData::Page($salons_participate,addons_url('Salon://Salon/MySalon/status/end'));
 
 		$salons_publish = M('e_salon')->where($data)->limit($salons_publish['offset'],$salons_publish['perpagenum'])->select();
 		$salons_participate= M('e_salon')->where($Pdata)->limit($salons_participate['offset'],$salons_participate['perpagenum'])->select();
@@ -235,13 +235,13 @@ class SalonController extends AddonsController{
 			$end_salons=M('e_salon')->where($data)->select();
 			var_dump($end_salons);
 			echo '1111111111';
-			$end_salons=\LfPageData::Page($end_salons,addons_url('Salon://Salon/SalonSquare/status/end'));
+			$this->EndPage=\LfPageData::Page($end_salons,addons_url('Salon://Salon/SalonSquare/status/end'));
 			$this->end_salons=M('e_salon')->where($data)->limit($end_salons['offset'],$end_salons['perpagenum'])->select();
 		}else{
 			$this->active1='active';
 			$salons=M('e_salon')->where($data)->select();
 			var_dump($salons);
-			$salons=\LfPageData::Page($salons,addons_url('Salon://Salon/SalonSquare/status/sign'));
+			$this->SignPage=\LfPageData::Page($salons,addons_url('Salon://Salon/SalonSquare/status/sign'));
 			$this->salons=M('e_salon')->where($data)->limit($salons['offset'],$salons['perpagenum'])->select();
 		}
 		echo $user->getLastSql();
